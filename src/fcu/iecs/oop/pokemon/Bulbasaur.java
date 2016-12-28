885d381cab0ea0bac42dd5db67ab3e6190baba81
user <user@MSI>
2016-12-28 23:16:42 +0800

    OOP-HW8

---
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -0,0 +1,17 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon{
+
+	public Bulbasaur(String nickName, PokemonType type, int cp) {
+		super(nickName, type, cp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("Tackle...");
+	}
+	
+	
+}
---
+++ src/fcu/iecs/oop/pokemon/Charmander.java
@@ -0,0 +1,16 @@
+package fcu.iecs.oop.pokemon;
+
+public class Charmander extends Pokemon{
+
+	public Charmander(String nickName, PokemonType type, int cp) {
+		super(nickName, type, cp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("Ember...");
+	}
+
+}
